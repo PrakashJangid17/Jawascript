472ee7799266ead7a6cdec4bd47b7dbdb9930d27
PrakashJangid17 <pj177606@gmail.com>
2025-03-29 17:08:43 +0000

    data type

---
+++ js_1/demo.js
@@ -0,0 +1,13 @@
+// let prakash = 234
+// const prveen ="ram"
+// var ashok = "dev"
+// aikta = "rrrr"
+// let bajrang = "ooo"
+// bajrang
+// console.table([prakash,prveen,ashok , aikta])
+
+
+console.log(null > 0)
+console.log(null >= 0)
+console.log(null == 0)
+console.log(null != 0)
