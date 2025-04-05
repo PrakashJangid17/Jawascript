522c8a09811f086cf52b5324af392992b7ae6a5d
PrakashJangid17 <pj177606@gmail.com>
2025-04-05 16:46:07 +0000

    foor loop

---
+++ js_1/scopes.js
@@ -0,0 +1,64 @@
+//=====glob scopes============== 
+
+let  a= 2;
+const b =3;
+var c = 4;
+//===== scopes============== 
+
+// if (condition) {
+//     let = 2;
+// const b =3;
+// var c = 4;
+// console.log(a)
+// }
+
+// console.log(a)
+// console.log(b)
+// console.log(c)
+
+
+//==================switch=======
+
+// const month = 4;
+
+// switch (month) {
+//     case 1:
+//         console.log("jau")
+//         break;
+//     case 2:
+//         console.log("fur")
+//         break;
+//     case 3:
+//         console.log("march")
+//         break;
+//     case 4:
+//         console.log("aprei")
+//         break;
+//     case 5:
+//         console.log("mai")
+//         break;
+
+//     default:
+//          console.log("deford chuse meatch");
+        
+//         break;
+// }
+
+
+//====================loop=====================
+
+// for (let i = 1; i <= 10; i++) {
+//     const element = i;
+//     if (element != 8) {
+//         console.log(`"good choisha number ", ${i}`)
+//     }else{
+//         console.log("bede ")
+//     }
+//     console.log(element) 
+    
+// }
+
+for (let i = 1; i <= 10; i++) {
+    const element = i * 2;
+    console.log(element); // prints 0, 5, 10, ..., 45
+}
